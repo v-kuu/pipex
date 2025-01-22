e7625be026ade1e6a5bf3100fadc53d7d8f96571
Ville Kuusela <vkuusela@student.hive.fi>
2025-01-22 14:21:49 +0200

    scaffolding

---
+++ pipex.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vkuusela <vkuusela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 14:20:32 by vkuusela          #+#    #+#             */
+/*   Updated: 2025/01/22 14:21:30 by vkuusela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	main(int argc, char **argv)
+{
+	// init pipex
+	// check args
+	// parse cmds
+	// parse args
+	// while cmds, exec
+	// cleanup
+}
